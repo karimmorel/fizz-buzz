dc064844a02d72acd470e1f813f8c353cd992652
Karim <karim-morel@outlook.fr>
2020-09-03 12:01:33 +0200

    Fizz Buzz

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Fizz Buzz
+I just discovered the Fizz Buzz interview question and I thinking about how I would answer to this question if I was asked to.
+
+I know that my solution is not the best in term of algorithm complexity (here O(n*m)), but it allows us to add as many "FizzBuzz" elements we want and to choose the number of iteration for the loop.
+Also, it uses an object oriented structure. 
---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+class FizzBuzz {
+
+    private $entries;
+    private $iterations;
+
+    public function __construct (array $entries, int $iterations)
+    {
+        $this->entries = $entries;
+        $this->iterations = $iterations;
+    }
+
+    public function fizzBuzz()
+    {
+        $output = "";
+
+        for ($i = 1; $i <= $this->iterations; $i++)
+        {
+            $outputFizz = false;
+
+            foreach($this->entries as $fizz => $modulo)
+            {
+                if($i % $modulo == 0)
+                {
+                    $output = $output.$fizz;
+                    $outputFizz = true;
+                }
+            }
+
+            if($outputFizz == false)
+            {
+                $output = $output.$i;
+            }
+
+            $output = $output."<br/>";
+
+        }
+
+        echo $output;
+    }
+
+}
+
+$entries = array('Fizz' => 3, 'Buzz' => 5, 'Bizz' => 17, 'Fuzz' => 255, 'Loop' => 5);
+
+$fizz = new FizzBuzz($entries, 2400);
+
+$fizz->fizzBuzz();
